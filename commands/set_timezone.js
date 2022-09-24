d55b358c8f6052423f8fff06fbfa7824cc9cc203
Ramen <rhuangcnnz@gmail.com>
2022-09-24 01:47:37 -0700

    now user can pick country if multiple cities with the same name are found

--- commands/set_timezone.js
+++ commands/set_timezone.js
@@ -16,12 +16,40 @@ module.exports = {
 		),
 	async execute(interaction) {
 		let cityName = interaction.options.getString('city');
-		const cityRes = cityTimezones.lookupViaCity(cityName);
+		// only get ten results to prevent too many choices for the user
+		const cityRes = cityTimezones.lookupViaCity(cityName).slice(0,10);
+		// if no city found
 		if(cityRes.length<1){
 			await interaction.reply(`City '${cityName}' not found`);
 			return;
 		}
-		let timezone = cityRes[0]['timezone'];
+		let timezone;
+		// if more than one result found
+		if(cityRes.length>1){
+			let cityOptions = "";
+			cityRes.forEach((element, index) => {
+				cityOptions += (index+1) + ": " + element["country"] + "\n";
+			});
+			await interaction.reply(cityOptions);
+			// set up for user's respond;
+			//const filter = m => interaction.user.id === m.author.id;
+			const filter = m => true;
+
+			await interaction.channel.awaitMessages({filter, time: 10000, max:1})
+				.then(messages => {
+					timezone = cityRes[parseInt(messages.first().content)-1]['timezone'];
+				})
+				.catch(() => {
+					interaction.followUp("No reply received, your time zone has not been set");
+				});
+		}else{
+			// if exactly one result found
+			timezone = cityRes[0]['timezone'];
+			await interaction.reply(timezone + " has been found.");
+		}
+		if(!timezone) {
+				return;
+		}
 		data[`${interaction.user.id}`]=timezone;
 
 		// write to database
@@ -30,6 +58,6 @@ module.exports = {
 			if(err) throw err;
 	});
 
-		await interaction.reply(`Your time zone has been set to ${timezone}`);
+		await interaction.followUp(`Your time zone has been set to ${timezone}`);
 	},
 };
--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { watimeTOKEN } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	] 
+});
 
 client.commands = new Collection();
 
